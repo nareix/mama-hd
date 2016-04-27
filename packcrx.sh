a7e28e420b89a892aa41e8296630752275b6ea62
nareix <hi.nareix@gmail.com>
2016-04-27 11:22:29 +0800

    add screenshot for webstore, update crx

--- packcrx.sh
+++ packcrx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -rf mama-hd.crx
+rm -rf mama-hd.crx mama-hd.zip
 webpack -d
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=`pwd`/mama-hd --pack-extension-key=`pwd`/mama-hd.pem
+zip mama-hd.zip -r mama-hd
 
