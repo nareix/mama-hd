ae01b539ac988bfbe0c4064907f904fca7a60dfc
nareix <hi.nareix@gmail.com>
2016-04-30 23:47:06 +0800

    fix #4

--- background.js
+++ background.js
@@ -4,10 +4,24 @@ chrome.browserAction.onClicked.addListener(function(){
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	function(details) {
-		return {requestHeaders: [
-			{name:'Referer', value:'http://static.youku.com/'},
-			{name:'Cookie', value:'__ysuid'+new Date().getTime()/1e3},
-		]};
+		var sethdr = {};
+		for (var i = 0; i < details.requestHeaders.length; i++) {
+			var header = details.requestHeaders[i];
+			if (header.name.substr(0,7) == 'sethdr-') {
+				sethdr[header.name.substr(7)] = header.value;
+			}
+		}
+		for (var i = 0; i < details.requestHeaders.length; i++) {
+			var header = details.requestHeaders[i];
+			if (sethdr[header.name]) {
+				header.value = sethdr[header.name];
+				delete sethdr[header.name];
+			}
+		}
+		for (var k in sethdr) {
+			details.requestHeaders.push({name:k, value:sethdr[k]});
+		}
+		return {requestHeaders: details.requestHeaders};
 	},
 	{urls: ["http://play.youku.com/*"]},
 	["blocking", "requestHeaders"]
