81d057207d24dac45e6a0c690fcb5730e42cb589
nareix <hi.nareix@gmail.com>
2016-05-09 21:39:26 +0800

    add tudou danmu getter

--- tudou.js
+++ tudou.js
@@ -5,6 +5,7 @@
 // cannot play http://www.tudou.com/programs/view/TXBFQYX6F04/ missing vcode
 
 var youku = require('./youku')
+var querystring = require('querystring');
 
 exports.testUrl = function (url) {
   return /tudou\.com/.test(url);
@@ -19,5 +20,22 @@ exports.getVideos = function (url) {
 		if (vcode) 
 			return youku.getVideosByVcode(vcode[1]);
 	})
+}
+
+exports.getDanmuRaw = function(id, params) {
+	//http://service.danmu.tudou.com/list?7122
+	//FormData: uid=81677981&mcount=1&iid=132611501&type=1&ct=1001&mat=6
+	//mat=minute at
+	params.uid = params.uid || 0;
+	params.mcount = params.mcount || 1;
+	params.type = params.type || 1;
+	params.ct = params.ct || 1001;
+	var body = querystring.stringify(params);
+	return fetch('http://service.danmu.tudou.com/list?'+id, {
+		credentials: 'include', body, method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+	}).then(res => res.json());
 }
 
