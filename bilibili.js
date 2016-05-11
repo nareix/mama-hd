b3421921a86fe68ba122e4363071e62eb72acf88
nareix <hi.nareix@gmail.com>
2016-05-11 23:46:31 +0800

    增加B站弹幕

--- bilibili.js
+++ bilibili.js
@@ -1,9 +1,9 @@
 
-module.exports.testUrl = url => url.match('bilibili.com/')
+exports.testUrl = url => url.match('bilibili.com/')
 
-module.exports.getVideos = function (url) {
+exports.getVideos = (url) => {
 	return fetch(url, {credentials: 'include'}).then(res => res.text()).then(res => {
-		var cid = res.match(/cid=(\d+)/);
+		let cid = res.match(/cid=(\d+)/);
 		if (cid)
 			return cid[1];
 	}).then(function(cid) {
@@ -11,12 +11,12 @@ module.exports.getVideos = function (url) {
 			return;
 		return fetch("http://interface.bilibili.com/playurl?appkey=8e9fc618fbd41e28&cid="+cid)
 		.then(res => res.text()).then(res => {
-			var parser = new DOMParser();
-			var doc = parser.parseFromString(res, 'text/xml');
-			var array = x => Array.prototype.slice.call(x);
-			var duration = 0.0;
+			let parser = new DOMParser();
+			let doc = parser.parseFromString(res, 'text/xml');
+			let array = x => Array.prototype.slice.call(x);
+			let duration = 0.0;
 			array(doc.querySelectorAll('durl > length')).forEach(len => duration += +len.textContent);
-			var src = array(doc.querySelectorAll('durl > url')).map(url => url.textContent );
+			let src = array(doc.querySelectorAll('durl > url')).map(url => url.textContent );
 			return {
 				duration: duration/1000.0,
 				src: src,
@@ -24,5 +24,38 @@ module.exports.getVideos = function (url) {
 			}
 		})
 	});
+}
+
+function pad(num, n) { 
+	if (num.length >= n)
+		return num;
+	return (Array(n).join(0) + num).slice(-n)
+}
+let colorDec2Hex = (x) => '#'+pad((x||0).toString(16), 6)
+exports.colorDec2Hex = colorDec2Hex;
+
+let getDamooRaw = url => {
+	return fetch(url).then(res => res.text()).then(res => {
+		let parser = new DOMParser();
+		let doc = parser.parseFromString(res, 'text/xml');
+		let array = x => Array.prototype.slice.call(x);
+		return array(doc.querySelectorAll('i > d')).map((d, i) => {
+			let p = d.getAttribute('p').split(',');
+			if (p[5] == 2)
+				return;
+			let pos;
+			switch (p[1]) {
+				case 4: pos = 'bottom'; break;
+				case 5: pos = 'top'; break;
+			}
+			return {time: parseFloat(p[0]), pos, color:colorDec2Hex(p[3]), text: d.textContent};
+		}).filter(x => x).sort((a,b) => a.time-b.time);
+		return arr;
+	})
+}
+exports.getDamooRaw = getDamooRaw;
+
+exports.getAllDamoo = (res) => {
+	return getDamooRaw(res.commentUrl);
 }
 
