e3cfd106fcfb4e7d41ec7296a884872ad6b8727b
nareix <hi.nareix@gmail.com>
2016-05-14 19:12:39 +0800

    给player增加右下角的设置框

--- index.js
+++ index.js
@@ -34,21 +34,72 @@ let tudou = require('./tudou');
 let createPlayer = require('./player');
 let flashBlocker = require('./flashBlocker');
 let flvdemux = require('./flvdemux');
-let Damoo = require('./damoo');
+let Damoo = require('./fastdamoo');
 
 let nanobar = new Nanobar();
 
 let style = document.createElement('style');
+let themeColor = '#DF6558';
+
 style.innerHTML = `
 .nanobar .bar {
-	background: #c16c70;
+	background: ${themeColor};
 }
 .nanobar {
-	z-index: 2999999;
+	z-index: 1000001;
 	left: 0px;
 	top: 0px;
 }
+.mama-toolbar {
+	position: absolute;
+	z-index: 1;
+	bottom: 20px;
+	right: 20px;
+}
+
+.mama-toolbar svg {
+	width: 17px;
+	color: #fff;
+	fill: currentColor;
+	cursor: pointer;
+}
+
+.mama-toolbar {
+	display: flex;
+	padding: 5px;
+	padding-left: 15px;
+	padding-right: 15px;
+	border-radius: 5px;
+	align-items: center;
+	background: #333;
+}
+
+.mama-toolbar input[type=range]:focus {
+  outline: none;
+}
+
+.mama-toolbar .selected {
+	color: ${themeColor};
+}
+
+.mama-toolbar input[type=range] {
+	-webkit-appearance: none;
+  height: 9px;
+	width: 75px;
+	border-radius: 3px;
+	margin: 0;
+	margin-right: 8px;
+}
+
+.mama-toolbar input[type=range]::-webkit-slider-thumb {
+	-webkit-appearance: none;
+	height: 13px;
+	width: 5px;
+	background: ${themeColor};
+	border-radius: 1px;
+}
 `
+
 document.head.appendChild(style);
 mediaSource.debug = true;
 
@@ -97,11 +148,10 @@ let handleDamoo = (vres, player, seeker, media) => {
 			});
 		}
 	})().then(() => {
-		//console.log(`damoo: loaded n=${damoos.length}`);
-
 		let video = player.video;
 		let updating;
 		let cur = 0;
+		let emitter;
 
 		let update = () => {
 			let time = video.currentTime+1.0;
@@ -109,7 +159,7 @@ let handleDamoo = (vres, player, seeker, media) => {
 				for (; cur < damoos.length && damoos[cur].time <= time; cur++) {
 					let d = damoos[cur];
 					console.log('damoo: emit', `${Math.floor(d.time/60)}:${Math.floor(d.time%60)}`, d.text);
-					//emitter.emit({text: d.text, shadow: true, color: d.color});
+					emitter.emit({text: d.text, pos: d.pos, shadow: {color: '#000'}, color: d.color});
 				}
 			}
 			updating = setTimeout(update, 1000);
@@ -138,16 +188,31 @@ let handleDamoo = (vres, player, seeker, media) => {
 
 		media.onSeek.push(() => {
 			//console.log('damoo: clear');
-			// emitter.clear();
+			emitter.clear();
 			resetCur();
 		})
 
 		player.onResume.push(() => {
-			// emitter.resume()
+			if (emitter == null) {
+	 			emitter = new Damoo({container:player.damoo, fontSize:25});
+			}
+			player.damoo.style.opacity = player.damooOpacity;
+			emitter.resume()
 			startUpdate();
+
+			let setDamooOpts = () => {
+				player.damoo.style.opacity = player.damooOpacity;
+				if (player.damooEnabled) {
+					emitter.show();
+				} else {
+					emitter.hide();
+				}
+			}
+			player.onDamooOptsChange.push(() => setDamooOpts());
+			setDamooOpts();
 		});
 		player.onSuspend.push(() => {
-			// emitter.suspend()
+			emitter.suspend()
 			stopUpdate();
 		});
 
@@ -356,24 +421,149 @@ cmd.testInitSegment = () => {
 	});
 }
 
+cmd.testPlayer = () => {
+	let player = createPlayer();
+	player.video.src = localhost+'projectindex.mp4';
+	player.video.muted = true;
+}
+//cmd.testPlayer();
+
+cmd.testCanvasSpeed = () => {
+	let canvas = document.createElement('canvas');
+	canvas.width = 1280;
+	canvas.height = 800;
+	let ctx = canvas.getContext('2d');
+
+	let line = [];
+	for (let i = 0; i < 3; i++) {
+		let c = document.createElement('canvas');
+		c.width = 100;
+		c.height = 100;
+		line.push(c);
+	}
+	console.log('canvas', canvas.width, canvas.height);
+
+	var _RAF = window.requestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			function(cb) { return setTimeout(cb, 17); };
+
+	setInterval(() => {
+		line.forEach(c => {
+			ctx.drawImage(c, 0, 0);
+			console.log('draw');
+		});
+	}, 1000/24);
+}
+
+cmd.testCssTransition = () => {
+	let freelist = [];
+	const FONTSIZE = 25;
+	const ROWS = 50;
+	let currow = 0;
+
+	let container = document.createElement('div');
+	container.style.width = '100%';
+	container.style.height = '100%';
+	document.body.appendChild(container);
+
+	for (let i = 0; i < 200; i++) {
+		let p = document.createElement('canvas');
+		p.width = 1;
+		p.height = 1;
+		p.style.position = 'absolute';
+		p.style.backgroundColor = 'transparent';
+		container.appendChild(p);
+		freelist.push(p);
+	}
+
+	let emit = ({text, pos, color, shadow}) => {
+		if (freelist.length == 0)
+			return;
+
+		currow++;
+		if (currow > ROWS)
+			currow = 0;
+
+		color = color || '#fff';
+		shadow = shadow || {color: '#000'};
+		pos = pos || 'normal';
+
+		let p = freelist[0];
+		freelist = freelist.slice(1);
+
+		let size = FONTSIZE;
+		let ctx = p.getContext('2d');
+		ctx.font = `${size}px Arail`;
+		p.width = ctx.measureText(text).width;
+		p.height = size*1.5;
+
+		ctx.font = `${size}px Arail`;
+		ctx.fillStyle = color;
+		ctx.textAlign = "start";
+		ctx.textBaseline = "top";
+		if (shadow) {
+			ctx.shadowOffsetX = 1;
+			ctx.shadowOffsetY = 1;
+			ctx.shadowColor = shadow.color;
+		}
+		ctx.fillText(text, 0, 0);
+
+		let time = 5;
+		let movew = container.offsetWidth+p.width;
+
+		p.style.top = `${currow*FONTSIZE}px`;
+
+		if (pos == 'top') {
+			p.style.left = `${(container.offsetWidth-p.width)/2}px`;
+		} else {
+			p.style.right = `${-p.width}px`;
+		}
+
+		p.style.display = 'block';
+
+		setTimeout(() => {
+			p.style.display = 'none';
+			freelist.push(p);
+		}, time*1000);
+
+		if (pos == 'normal') {
+			setTimeout(() => {
+				p.style.transition = `transform ${time}s linear`;
+				p.style.transform = `translate(-${movew}px,0)`;
+			}, 50);
+		}
+
+	}
+
+	emit({text:'哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩哔哩'});
+	emit({text:'我占了中间位置', color:'#f00', pos:'top'});
+	//setInterval(reset, 2000);
+}
+
 cmd.testDamoo = () => {
 	let div = document.createElement('div');
+
+	document.body.style.margin = '0';
+	let resize = () => {
+		div.style.height = window.innerHeight+'px';
+		div.style.width = window.innerWidth+'px';
+	}
+	window.addEventListener('resize', () => resize());
+	resize();
 
 	div.innerHTML = `
 		<h1>Background</h1>
 	`;
-	div.style.height = '100%';
-	div.style.width = '100%';
 	div.style.background = '#eee';
-
 	document.body.appendChild(div);
 
-	let dm = new Damoo(div, 'test', 20);
-	dm.start();
-
+	let dm = new Damoo({container:div, fontSize:20});
 	dm.emit({text:'小小小的文字', color:'#000'});
-	dm.emit({text:'小小小的文字', color:'#000', fixed:true});
-	dm.emit({text:'稍微长一点的文字2333333333333333333', color:'#000', fixed:true});
+	dm.emit({text:'小小小的文字', color:'#000', pos:'bottom'});
+	dm.emit({text:'稍微长一点的文字2333333333333333333', color:'#000', pos:'top'});
 
 	let i = 0;
 	let timer = setInterval(() => {
@@ -388,10 +578,10 @@ cmd.testDamoo = () => {
 		dm.emit({
 			text,
 			color: '#f00', 
-			shadow: true,
 		});
 	}, 100);
 }
+//cmd.testDamoo()
 
 if (location.href.substr(0,6) != 'chrome') {
 	playUrl(location.href);
--- player.js
+++ player.js
@@ -1,25 +1,76 @@
 
 module.exports = () => {
 	let div = document.createElement('div');
-	div.innerHTML = `<video></video>`;
+	let damoo = document.createElement('div');
+	let video = document.createElement('video');
+
+	let toolbar = document.createElement('div');
+	toolbar.className = 'mama-toolbar';
+	toolbar.innerHTML += `<input class="damoo-input" type="range" />` 
+	toolbar.innerHTML += `<svg version="1.1" 
+		class="damoo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 60 60" style="enable-background:new 0 0 60 60;" xml:space="preserve">
+		<path d="M6,2h48c3.252,0,6,2.748,6,6v33c0,3.252-2.748,6-6,6H25.442L15.74,57.673C15.546,57.885,15.276,58,15,58
+			c-0.121,0-0.243-0.022-0.361-0.067C14.254,57.784,14,57.413,14,57V47H6c-3.252,0-6-2.748-6-6L0,8C0,4.748,2.748,2,6,2z"/>
+		</svg>
+	`;
+	toolbar.style.display = 'none';
+
+	div.appendChild(toolbar);
+	div.appendChild(video);
+	div.appendChild(damoo);
+
 	div.style.background = '#000';
 	div.style.position = 'fixed';
 	div.style.top = '0px';
 	div.style.left = '0px';
-	div.style.zIndex = '999999';
+	div.style.zIndex = '1000000';
 
-	let video = div.querySelector('video');
+	damoo.style.position = 'absolute';
+	damoo.style.pointerEvents = 'none';
+	damoo.style.overflow = 'hidden';
+
 	video.autoplay = true;
 	video.controls = true;
 	video.style.position = 'absolute';
 	video.style.display = 'none';
 
-	let self = {video, div, onStarted: [], onSuspend: [], onResume: []};
+	function debounce(start, end, interval) {
+		var timer;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timer = null;
+				end.apply(context, args);
+			};
+			if (timer) {
+				clearTimeout(timer);
+			} else {
+				start.apply(context, args);
+			}
+			timer = setTimeout(later, interval);
+		};
+	};
+
+	div.addEventListener('mousemove', debounce(() => {
+		div.style.cursor = 'default';
+		toolbar.style.display = 'flex';
+	}, () => {
+		div.style.cursor = 'none';
+		toolbar.style.display = 'none';
+	}, 5000));
+
+	let self = {
+		video, damoo, div,
+		onStarted:[], onSuspend:[], onResume:[],
+		damooEnabled:false,
+		damooOpacity:1.0,
+		onDamooOptsChange:[],
+	};
 
 	let resize = () => {
 		let windowRatio = window.innerHeight/window.innerWidth;
 		let videoRatio = video.videoHeight/video.videoWidth;
-
 		if (videoRatio > windowRatio) {
 			let width = window.innerHeight/videoRatio;
 			video.style.height = window.innerHeight+'px';
@@ -33,7 +84,10 @@ module.exports = () => {
 			video.style.top = (window.innerHeight-height)/2+'px';
 			video.style.left = '0px';
 		}
-
+		damoo.style.width = video.style.width;
+		damoo.style.height = video.style.height;
+		damoo.style.top = video.style.top;
+		damoo.style.left = video.style.left;
 		div.style.height = window.innerHeight+'px';
 		div.style.width = window.innerWidth+'px';
 	}
@@ -62,16 +116,16 @@ module.exports = () => {
 		let setToPaused = () => {
 			if (playing) {
 				playing = false;
-				self.onSuspend.forEach(x => x());
 				console.log('player: suspend');
+				self.onSuspend.forEach(x => x());
 			}
 		}
 
 		let setToPlaying = () => {
 			if (!playing) {
 				playing = true;
-				self.onResume.forEach(x => x());
 				console.log('player: resume');
+				self.onResume.forEach(x => x());
 			}
 		}
 
@@ -98,29 +152,9 @@ module.exports = () => {
 		}
 	}
 
-	function doubleclick(el, onsingle, ondouble) {
-		let dbl;
-		return () => {
-			if (!dbl) {
-				dbl = true;
-				setTimeout(function () {
-					if (dbl) {
-						onsingle();
-					}
-					dbl = false;
-				}, 300);
-			} else {
-				dbl = false;
-				ondouble();
-			}
-		}
-	}
-
-	video.addEventListener('mousedown', doubleclick(video, () => {
+	video.addEventListener('mousedown', () => {
 		togglePlayPause();
-	}, () => {
-		toggleFullScreen();
-	}))
+	})
 
 	let seekDelta = 5.0;
 	let getSeekTime = (delta) => {
@@ -146,6 +180,10 @@ module.exports = () => {
 
 	let volumeDelta = 0.2;
 	let volumeUp = () => {
+		if (video.muted) {
+			video.muted = false;
+			video.volume = 0.0;
+		}
 		video.volume = Math.min(1.0, video.volume+volumeDelta);
 	}
 	let volumeDown = () => {
@@ -154,6 +192,39 @@ module.exports = () => {
 
 	let toggleMute = () => {
 		video.muted = !video.muted;
+	}
+
+	let toggleDamoo;
+	{
+		let btn = toolbar.querySelector('.damoo');
+		let input = toolbar.querySelector('.damoo-input');
+		input.min = 0;
+		input.max = 1;
+		input.step = 0.01;
+		input.value = self.damooOpacity;
+		if (self.damooEnabled) {
+			btn.classList.add('selected');
+			input.style.display = 'block';
+		} else {
+			input.style.display = 'none';
+		}
+		toggleDamoo = () => {
+			btn.classList.toggle('selected');
+			self.damooEnabled = !self.damooEnabled;
+			if (self.damooEnabled) {
+				input.style.display = 'block';
+			} else {
+				input.style.display = 'none';
+			}
+			self.onDamooOptsChange.forEach(x => x());
+		}
+		btn.addEventListener('click', () => {
+			toggleDamoo();
+		});
+		input.addEventListener('change', () => {
+			self.damooOpacity = input.value;
+			self.onDamooOptsChange.forEach(x => x());
+		});
 	}
 
 	document.body.addEventListener('keydown', (e) => {
@@ -165,6 +236,11 @@ module.exports = () => {
 
 			case "ArrowUp": {
 				volumeUp();
+				e.preventDefault();
+			} break;
+
+			case "KeyD": {
+				toggleDamoo();
 				e.preventDefault();
 			} break;
 
